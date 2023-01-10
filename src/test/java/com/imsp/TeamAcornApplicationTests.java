0fd36822c434882c46c8b344510f9787f7a2df68
AppleLee0423 <ld210@naver.com>
2023-01-10 22:07:14 +0900

    first commit

---
+++ src/test/java/com/imsp/TeamAcornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAcornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
